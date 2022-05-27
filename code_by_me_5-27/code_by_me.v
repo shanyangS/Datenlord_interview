5587dd832d052b6b72e589d4198b3f44b8ea1746
ShanDaYang <shanyangs@outlook.com>
2022-05-28 00:04:10 +0800

    break valid and ready
    1

--- code_by_me_5-27/code_by_me.v
+++ code_by_me_5-27/code_by_me.v
@@ -18,22 +18,36 @@ module cy_skidbuffer
 reg			r_valid;
 reg[DW-1:0]		r_data;
 
-/*r_valid section */
+
+/* valid sequential section */
 initial	r_valid = 0;
 always@(posedge i_clk)
 begin
 if(i_reset)
 	r_valid <= 0;
-else if((i_valid && o_ready) && (o_valid && !i_ready))	//have incoming data, but the output	is stalled
+else if((i_valid && o_ready) && (o_valid && !i_ready))	//have incoming data, but the output is stalled
 	r_valid <= 1;
 else if(i_ready)	//data has already go
 	r_valid <= 0;
 end
-/* da pai */
+/*
+always@(posedge clk)
+	if((i_valid && o_ready) && (o_valid && !i_ready))
+	o_ready = 0;
+	else if(i_ready)
+	o_ready = 1;
+*/
+/* ready sequential section */
 always@(*)
 	o_ready = !r_valid;  //o_ready to make the i_ready fit the sequence
 
-/* o_valid section */
+////////////////////////////////////////////////////////////////////////////////
+
+/* when the data come in,copy it. */
+always@(posedge i_clk) 
+if(i_valid && o_ready) 
+	r_data <= i_data;
+
 initial	o_valid = 0;	//reg out
 always@(posedge i_clk)
 begin
@@ -42,21 +56,15 @@ if(i_reset)
 else if (!o_valid || i_ready)
 	o_valid <= (i_valid || r_valid);
 end
-////////////////////////////////////////////////////////////////////////////////
-
-/* when the data come in,copy it. */
-always@(posedge i_clk) 
-if(i_valid && o_ready) 
-	r_data <= i_data;
 
 initial	o_data = 0;	//reg out
 always @(posedge i_clk)
 begin
 	if (i_reset)
 		o_data <= 0;
-	else if (!o_valid || i_ready)	//reg_out
+	else if (!o_valid || i_ready)	//reg_out //"!(o_valid && !i_ready)"
 		begin
-		if(r_valid)
+		if(r_valid)	
 			o_data <= r_data;
 		else
 			o_data <= i_data;
